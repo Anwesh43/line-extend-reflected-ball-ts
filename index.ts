b302be7668cbe31a182489965a967864d4da88e2
Anwesh43 <anweshthecool0@gmail.com>
2020-09-13 17:59:23 +0530

    adding DrawingUtil class

--- index.ts
+++ index.ts
@@ -11,6 +11,8 @@ const strokeFactor : number = 90
 const sizeFactor : number = 13.2 
 const delay : number = 20 
 const backColor : string = "#bdbdbd"
+const w : number = window.innerWidth 
+const h : number = window.innerHeight
 
 class ScaleUtil {
 
@@ -24,5 +26,41 @@ class ScaleUtil {
 
     static sinify(scale : number) : number {
         return Math.sin(scale * Math.PI)
+    }
+}
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawCircle(context : CanvasRenderingContext2D, x : number, y : number, r : number) {
+        context.beginPath()
+        context.arc(x, y, r, 0, 2 * Math.PI)
+        context.fill()
+    }
+
+    static drawLineExtendedBall(context : CanvasRenderingContext2D, scale : number) {
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, parts)
+        const sc2 : number = ScaleUtil.divideScale(scale, 1, parts)
+        const sc3 : number = ScaleUtil.divideScale(scale, 2, parts)
+        const sf2 : number = ScaleUtil.sinify(sc2)        
+        const r : number = Math.min(w, h) / sizeFactor 
+        context.save()
+        DrawingUtil.drawCircle(context, r + (w - 2 * r) * sc2, r + (h / 2 - r) * sf2, r * (sc1 - sc3))
+        DrawingUtil.drawLine(context, w * sc3, h / 2, w * sc1, h / 2)
+        context.restore()
+    } 
+
+    static drawLEBNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        context.fillStyle = colors[i]
+        DrawingUtil.drawLineExtendedBall(context, scale)
     }
 }
